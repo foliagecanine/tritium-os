28f4a35a6e17b1a85fdf03a7774d46284eb01c79
foliagecanine <45408062+foliagecanine@users.noreply.github.com>
2019-06-30 10:04:21 -0700

    Fixed  a few things
    Also got rid of unnecessary code

--- add-o-file.sh
+++ add-o-file.sh
@@ -1,6 +1,6 @@
 if [ -e $1 ]; then
 echo HELP:
-echo ./add-c-file.sh \[filename\]
+echo ./add-o-file.sh \[filename\]
 echo filename: file name without extension \(e.g. \"hello\" rather than \"hello.c\"\)
 else
 echo \$\(ARCHDIR\)/$1.o \\ >> ./kernel/arch/i386/make.config
