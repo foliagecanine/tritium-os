b4ba8466b1b5b317006bb44f5e35f184ed357992
foliagecanine <45408062+foliagecanine@users.noreply.github.com>
2020-06-16 11:26:42 -0700

    Update game

--- prgms/advnture/main.c
+++ prgms/advnture/main.c
@@ -11,12 +11,30 @@ void yield() {
 	syscall(6);
 }
 
-#define NUM_COMMANDS 7
+typedef struct {
+	uint8_t x;
+	uint8_t y;
+	char *name;
+	bool aquired;
+} item;
+
+#define MAX_INV_ITEMS 16
+
+uint8_t inventory[MAX_INV_ITEMS];
+uint8_t num_inv_items = 0;
+
+item items[MAX_INV_ITEMS];
+bool doors[12];
+
+#define NUM_COMMANDS 10
 
 char *commands[NUM_COMMANDS] = {
 	"help",
 	"exit",
 	"room",
+	"inv",
+	"look {under/at} [object]",
+	"take [object]",
 	"north/n",
 	"south/s",
 	"east/e",
@@ -27,27 +45,30 @@ char *helptext[NUM_COMMANDS] = {
 	"Shows help",
 	"Exits program",
 	"Show info about the room",
-	"Go North",
-	"Go South",
-	"Go East",
-	"Go West"
+	"Show items in your inventory",
+	"Look under/at object specified",
+	"Grab the specified object",
+	"Go north",
+	"Go south",
+	"Go east",
+	"Go west"
 };
 
 char *cr_texts[3][3] = {
 	{
-		"You are in a room with no doors because the programmer was too lazy to put them in.",
-		"Error",
-		"Error",
+		"You are in a stone room with a red carpet and door to the north.",
+		"You are in an empty room. There is a door to the south.",
+		"Error: no room text",
 	},
 	{
-		"Error",
-		"Error",
-		"Error",
+		"You are in an empty room. There is a door to the west.",
+		"Error: no room text",
+		"Error: no room text",
 	},
 	{
-		"Error",
-		"Error",
-		"Error",
+		"Error: no room text",
+		"Error: no room text",
+		"Error: no room text",
 	}
 };
 
@@ -56,6 +77,36 @@ uint8_t index = 0;
 char *current_room_text;
 uint8_t cr_y = 0;
 uint8_t cr_x = 0;
+
+bool has(uint8_t iid) {
+	for (uint8_t i = 0; i < MAX_INV_ITEMS; i++) {
+		if (inventory[i]==iid)
+			return true;
+	}
+	return false;
+}
+
+uint8_t give_item(uint8_t iid) {
+	for (uint8_t i = 0; i < MAX_INV_ITEMS; i++) {
+		if (inventory[i]==0xFF) {
+			inventory[i]=iid;
+			items[iid].aquired = true;
+			num_inv_items++;
+			return i;
+		}
+	}
+	return 0xFF;
+}
+
+uint8_t remove_item(uint8_t iid) {
+	for (uint8_t i = 0; i < MAX_INV_ITEMS; i++) {
+		if (inventory[i]==iid) {
+			inventory[i]=0xFF;
+			num_inv_items--;
+			return i;
+		}
+	}
+}
 
 uint8_t run() {
 	printf("> ");
@@ -86,8 +137,113 @@ uint8_t run() {
 		}
 	}
 	
+	if (cr_x==2&&cr_y==2) {
+		
+	}
+	
+	if (cr_x==2&&cr_y==1) {
+		
+	}
+	
+	if (cr_x==2&&cr_y==0) {
+		
+	}
+	
+	if (cr_x==1&&cr_y==2) {
+		
+	}
+	
+	if (cr_x==1&&cr_y==1) {
+		
+	}
+	
+	if (cr_x==1&&cr_y==0) {
+		if (strcmp(cmd,"w")) {
+			printf("You walk west\n");
+			cr_x--;
+		}
+	}
+	
+	if (cr_x==0&&cr_y==2) {
+		
+	}
+	
+	if (cr_x==0&&cr_y==1) {
+		if (strcmp(cmd,"s")) {
+			printf("You walk south\n");
+			cr_y--;
+		}
+	}
+	
+	if (cr_x==0&&cr_y==0) {
+		if (strcmp(cmd,"n")||strcmp(cmd,"north")) {
+			if (doors[1]||has(0)) {
+				if (has(0)) {
+					printf("You insert the key.\nIt unlocks, but the key won't come out.\n");
+					remove_item(0);
+				}
+				doors[1]=true;
+				cr_y++;
+				printf("You walk north\n");
+			} else {
+				printf("That door is locked.\n");
+			}
+		}
+		if (strcmp(cmd,"e")&&doors[0]) {
+			printf("You walk east\n");
+			cr_x++;
+		}
+		if (strcmp(cmd,"look under carpet")&&!items[0].aquired) {
+			printf("You look under the carpet and find a red key.\nYou take the key.\n");
+			give_item(0);
+		}
+		if (strcmp(cmd,"look at carpet")) {
+			if (!items[0].aquired)
+				printf("A red carpet with a lump in the middle.\n");
+			else
+				printf("It's a red carpet.\n");
+		}
+		if (strcmp(cmd,"look at wall")) {
+			cr_texts[0][0]=	"You are in a stone room with a red carpet, a door to the east, and a door to the north.",
+			printf("You find a button on the wall and press it. A door to the east opens.\n");
+			doors[0]=true;
+		}
+	}
+	
 	if (strcmp(cmd,"room")) {
 		printf("%s\n",current_room_text);
+	}
+	
+	if (strcmp(cmd,"hello")) {
+		printf("You hear a voice out of nowhere: \"Hello there.\"\nYou can't find who said it.\n");
+	}
+	
+	if (strcmp(cmd,"inv")) {
+		printf("You have ");
+		uint8_t items_found = 0;
+		if (num_inv_items==0) {
+			printf("no items");
+		} else if (num_inv_items==1) {
+			for (uint8_t i = 0; i < MAX_INV_ITEMS; i++) {
+				if (has(i)) {
+					printf("%s",items[0].name);
+					break;
+				}
+			}
+		} else {
+			for (uint8_t i = 0; i < MAX_INV_ITEMS; i++) {
+				if (has(i)) {
+					if (items_found!=num_inv_items-1) {
+						printf("%s, ",items[i].name);
+						items_found++;
+					} else {
+						printf("and %s",items[i].name);
+						break;
+					}
+				}
+			}
+		}
+		printf(" in your inventory.\n");
 	}
 	
 	if (strcmp(cmd,"help")) {
@@ -108,12 +264,15 @@ uint8_t run() {
 _Noreturn void main() {
 	terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_GREY,VGA_COLOR_BLACK));
 	printf("Welcome to Text Adventure!\n");
-	printf("This game is distributed with TritiumOS\n");
-	printf("under the MIT license.\n");
+	printf("This game is distributed with TritiumOS under the MIT license.\n");
 	printf("https://raw.githubusercontent.com/foliagecanine/tritium-os/master/LICENSE\n\n");
 	printf("Enter a command or type \"help\" for help\n\n");
-	current_room_text = cr_texts[cr_x][cr_y];
+	
+	memset(inventory,0xFF,16);
+	
+	items[0].name = "a red key";
+	
 	while(run())
-		;
+		current_room_text = cr_texts[cr_x][cr_y];
 	exit(0);
 }
