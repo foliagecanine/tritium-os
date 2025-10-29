29ea3177d92f4c408ac252950e55c8e1f481ab92
foliagecanine <45408062+foliagecanine@users.noreply.github.com>
2025-10-28 18:08:30 -0700

    Update documentation for ASM.PRG

--- prgms/asm/elf.c
+++ prgms/asm/elf.c
@@ -34,28 +34,56 @@ typedef struct {
 	uint32_t align;
 } __attribute__((packed)) elfph32;
 
-elfhdr32 default_elf_header = {.magic = {0x7F, 'E', 'L', 'F'}, .arch_class = 1, .arch_endian = 1, .file_version = 1, .os_abi = 0, .abi_version = 0, .pad = {0}, .type = 2, .arch_target = 3, .elf_version = 1, .entrypoint = 0x8040000, .ph_offset = sizeof(elfhdr32), .sh_offset = 0, .flags = 0, .header_size = sizeof(elfhdr32), .ph_size = sizeof(elfph32), .ph_num = 1, .sh_size = 0, .sh_num = 0, .sh_string_index = 0};
+elfhdr32 default_elf_header = {
+	.magic = {0x7F, 'E', 'L', 'F'},
+	.arch_class = 1,
+	.arch_endian = 1,
+	.file_version = 1,
+	.os_abi = 0,
+	.abi_version = 0,
+	.pad = {0},
+	.type = 2,
+	.arch_target = 3,
+	.elf_version = 1,
+	.entrypoint = 0x8040000,
+	.ph_offset = sizeof(elfhdr32),
+	.sh_offset = 0,
+	.flags = 0,
+	.header_size = sizeof(elfhdr32),
+	.ph_size = sizeof(elfph32),
+	.ph_num = 1,
+	.sh_size = 0,
+	.sh_num = 0,
+	.sh_string_index = 0
+};
 
-elfph32 default_program_header = {.type = 1,
-                                  .offset = sizeof(elfhdr32) + sizeof(elfph32),
-                                  .vaddr = 0x8040000,
-                                  .paddr = 0,
-                                  .filesize = 0, // ELF generator will fix this
-                                  .memsize = 0,  // ELF generator will fix this
-                                  .flags = 7,
-                                  .align = 0x1000};
+elfph32 default_program_header = {
+	.type = 1,
+	.offset = sizeof(elfhdr32) + sizeof(elfph32),
+	.vaddr = 0x8040000,
+	.paddr = 0, // Ignored by OS
+	.filesize = 0, // ELF generator will fill this in
+	.memsize = 0,  // ELF generator will fill this in
+	.flags = 7,
+	.align = 0x1000
+};
 
 void *encode_elf(void *in, size_t len_in, size_t *len_out) {
 	elfhdr32 new_elf_hdr;
 	elfph32  new_program_hdr;
+	
 	memcpy(&new_elf_hdr, &default_elf_header, sizeof(elfhdr32));
 	memcpy(&new_program_hdr, &default_program_header, sizeof(elfph32));
+	
 	new_program_hdr.filesize = len_in;
 	new_program_hdr.memsize = len_in;
+	
 	void *elf_data = malloc(sizeof(elfhdr32) + sizeof(elfph32) + len_in);
+	
 	memcpy(elf_data, &new_elf_hdr, sizeof(elfhdr32));
 	memcpy(elf_data + sizeof(elfhdr32), &new_program_hdr, sizeof(elfph32));
 	memcpy(elf_data + sizeof(elfhdr32) + sizeof(elfph32), in, len_in);
 	*len_out = sizeof(elfhdr32) + sizeof(elfph32) + len_in;
+	
 	return elf_data;
 }
--- prgms/asm/macros.h
+++ prgms/asm/macros.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include "estring.h"
 
+/**
+ * Encodes macros from the input estring into the output estring.
+ * 
+ * @param in Pointer to the input estring containing macros.
+ * @param len_out Pointer to a size_t variable where the length of the resulting encoded data will be stored.
+ * @return Pointer to the newly allocated encoded data. The caller is responsible for freeing this memory.
+ */
 void *encode_macros(estring *in, size_t *len_out);
 
 #endif
